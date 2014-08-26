c77f441059c6658f48ae0bc63842a4060dbc448a
Tom Brennan <tommybrennan@gmail.com>
2014-08-26 02:20:11 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudioMcgeeOrders::Application.config.secret_key_base = 'cdd5d92e85699b66d575c8d7578e1cd966462a70f3a768b3384598524a6915c97d37c7515ac5d6c7543b614977d9a3613f6911fb5bed354fe5caaa93e2fdc10a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StudioMcgeeOrders::Application.config.session_store :cookie_store, key: '_studio_mcgee_orders_session'
