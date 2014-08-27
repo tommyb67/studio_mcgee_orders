2c27300bdfa02d4e08e20344d0cf402ed38f9a3a
Tom Brennan <tommybrennan@gmail.com>
2014-08-27 17:09:57 -0400

    added the carrier field to the orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827063853) do
+ActiveRecord::Schema.define(version: 20140827195002) do
 
   create_table "orders", force: true do |t|
     t.string   "company"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140827063853) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "item_number"
+    t.string   "carrier"
   end
 
   create_table "users", force: true do |t|
