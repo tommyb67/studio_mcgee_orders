35c8fee6f481bff4468d3655a74db0a3bcd2a614
Tom Brennan <tommybrennan@gmail.com>
2014-08-29 14:50:28 -0400

    added authorization to the orders controller and updated the index view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /orders
   # GET /orders.json
@@ -14,7 +16,7 @@ class OrdersController < ApplicationController
 
   # GET /orders/new
   def new
-    @order = Order.new
+    @order = current_user.orders.build
   end
 
   # GET /orders/1/edit
@@ -24,7 +26,7 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-    @order = Order.new(order_params)
+    @order = current_user.orders.build(order_params)
 
     respond_to do |format|
       if @order.save
@@ -65,6 +67,11 @@ class OrdersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_order
       @order = Order.find(params[:id])
+    end
+
+    def correct_user
+      @order = current_user.orders.find_by(id: params[:id])
+      redirect_to orders_path, notice: "Not authorized to edit this pin" if @order.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
