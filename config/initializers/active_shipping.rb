87c752e2095ad23472311ee8e910a219932f6001
Tom Brennan <tommybrennan@gmail.com>
2014-09-06 16:56:26 -0400

    working on issues with the active shipping gem

--- config/initializers/active_shipping.rb
+++ config/initializers/active_shipping.rb
@@ -1,3 +1,6 @@
+require 'active_shipping'
+include ActiveMerchant::Shipping
+
 ActiveShipping.configure do |config|
   config.ups    = { :key      => ENV["UPS_KEY"],
                     :user_id  => ENV['UPS_USERID'],
